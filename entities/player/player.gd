a05cd30f1f89a77b8b71910fc6f49788225a6633
zetcc0 <carabellochinellato.juanpablo@gmail.com>
2025-07-10 20:04:01 -0300

    chores/bugfix(/sprite folder)
    move and delete some files and fix error with sprite folder and icon.svg

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -3,6 +3,6 @@ extends Node2D
 @onready var tank = $Tank
 @onready var cannon = $Cannon
 
-func _process(delta):
+func _process(_delta):
 	# Pegá el cañón al tanque, copiando su posición (pero NO su rotación)
 	cannon.global_position = tank.global_position
